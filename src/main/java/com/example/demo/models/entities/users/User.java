957043f672b1a397d4fc5ecb47f65862e9eb93fa
dduartess <dandandsoares@gmail.com>
2026-01-19 16:29:05 -0300

    Feat: Create constructor for create new User

--- src/main/java/com/example/demo/models/entities/users/User.java
+++ src/main/java/com/example/demo/models/entities/users/User.java
@@ -27,9 +27,13 @@ public class User implements UserDetails {
 
     private String login;
     private String password;
-    private String email;
     private UserRole role;
 
+    public User(String login, String password, UserRole role){
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
