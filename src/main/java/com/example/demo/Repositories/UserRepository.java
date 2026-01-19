c0cc243e1a1d3c40cd5b8cd4a2fc0af8e1ffd3fe
dduartess <dandandsoares@gmail.com>
2026-01-19 15:43:18 -0300

    Feat: Create method findByLogin in UserRepository

--- src/main/java/com/example/demo/Repositories/UserRepository.java
+++ src/main/java/com/example/demo/Repositories/UserRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.Repositories;
 
 import com.example.demo.models.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    UserDetails findByLogin(String login);
 }
